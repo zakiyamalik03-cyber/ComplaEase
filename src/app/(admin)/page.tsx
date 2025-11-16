2f1322b540d20648fe8b978e1050bfc77ae3883c
alihusnain-dev <alihusnainmughal215@gmail.com>
2025-11-16 14:27:54 +0500

    remove statics section from page.tsx

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import StatisticsChart from "@/components/ecommerce/StatisticsChart";
 import MonthlyComplaintChart from "@/components/ecommerce/MonthlyComplaintChart";
 import { useRouter } from "next/navigation";
 import ComplaintMetrics from "@/components/ecommerce/ComplaintMetrics";
