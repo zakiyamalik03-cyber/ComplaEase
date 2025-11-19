23654eae2703e06af8ce143281f41c24abdaef85
alihusnain-dev <alihusnainmughal215@gmail.com>
2025-11-19 21:11:09 +0500

    show the user name in the complaint list of dashboard

--- src/app/(admin)/(others-pages)/(tables)/complaints/[slug]/complaint-page/ComplaintDetailsCard.tsx
+++ src/app/(admin)/(others-pages)/(tables)/complaints/[slug]/complaint-page/ComplaintDetailsCard.tsx
@@ -104,7 +104,7 @@ export default function ComplaintDetailsCard({ complaint }: { complaint: Complai
           </div>
         </div>
 
-        <button
+        {/* <button
           onClick={openModal}
           className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
         >
@@ -124,7 +124,7 @@ export default function ComplaintDetailsCard({ complaint }: { complaint: Complai
             />
           </svg>
           Edit
-        </button>
+        </button> */}
       </div>
 
       <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
